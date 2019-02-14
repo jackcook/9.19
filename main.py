981ab08cf6d9617f8d81b31da88535193445cda9
Jack Cook <hello@jackcook.nyc>
2019-02-13 23:30:58 -0500

    Added stop words

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+from nltk.corpus import stopwords
 from nltk.tokenize.toktok import ToktokTokenizer
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import string
 from tqdm import tqdm
 
 tok = ToktokTokenizer()
+stop_words = stopwords.words("english") + list(string.punctuation)
 
 def train_naive_bayes(documents, classes):
     vocabulary = set()
@@ -12,6 +15,12 @@ def train_naive_bayes(documents, classes):
     for i in range(len(documents)):
         d_vocab = set(tok.tokenize(documents.iloc[i, :]["text"]))
         vocabulary.update(d_vocab)
+
+    for word in stop_words:
+        try:
+            vocabulary.remove(word)
+        except:
+            pass
 
     print("Loaded vocabulary, %d tokens" % len(vocabulary))
 
@@ -33,6 +42,8 @@ def train_naive_bayes(documents, classes):
 
             if rating == c:
                 text = tok.tokenize(documents.iloc[i, :]["text"])
+                text = [w for w in text if not w in stop_words]
+
                 big_doc[c].extend(text)
 
                 for word in text:
@@ -63,7 +74,7 @@ def train_naive_bayes(documents, classes):
 train_df = pd.read_csv("./data/train.csv")
 
 # Remove ten reviews with NaN text
-train_df["text"] = train_df["review/text"]
+train_df["text"] = train_df["review/text"].str.lower()
 train_df = train_df[train_df["text"].apply(lambda x: isinstance(x, str))]
 
 # Remove one review with 0 rating
@@ -75,7 +86,7 @@ train_df = train_df[["text", "rating"]]
 X = train_df["text"]
 y = train_df["rating"]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.1)
 train_df = pd.concat([X_train, y_train], axis=1)
 test_df = pd.concat([X_test, y_test], axis=1)
 
@@ -84,6 +95,7 @@ log_prior, log_likelihood, vocabulary = train_naive_bayes(train_df, [1, 1.5, 2, 
 def test_naive_bayes(document, log_prior, log_likelihood, classes, vocabulary):
     sum = {}
     document = tok.tokenize(document)
+    document = [w for w in document if not w in stop_words]
 
     for c in classes:
         sum[c] = log_prior[c]
@@ -116,3 +128,5 @@ for i in tqdm(range(len(test_df))):
     predicted.append(test(i))
 
 test_df["predicted"] = predicted
+test_df["squared_error"] = (test_df["rating"] - test_df["predicted"]) * (test_df["rating"] - test_df["predicted"])
+print("Mean squared error: %f" % (test_df["squared_error"].mean()))
